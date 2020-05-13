53c63140d6d530bbb87b5ac48a43cfc7dc1e9199
user.name <nicoescol0818@gmail.com>
2020-05-13 11:28:27 -0500

    TreeInfo class added

---
+++ olimpic-alghoritms/src/model/TreeInfo.java
@@ -0,0 +1,55 @@
+package model;
+
+public class TreeInfo {
+	
+	//attributes
+	
+	private long number;
+	
+	//relations
+	
+	private TreeInfo father;
+	private TreeInfo right;
+	private TreeInfo left;
+	
+	//methods
+	public TreeInfo(long n) {
+		number = n;
+	}
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
+	public TreeInfo getFather() {
+		return father;
+	}
+
+	public void setFather(TreeInfo father) {
+		this.father = father;
+	}
+
+	public TreeInfo getRight() {
+		return right;
+	}
+
+	public void setRight(TreeInfo right) {
+		this.right = right;
+	}
+
+	public TreeInfo getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeInfo left) {
+		this.left = left;
+	}
+	
+	public boolean isMyNumber(long n) {
+		return number == n;
+	}
+}
