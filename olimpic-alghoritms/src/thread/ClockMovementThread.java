b47109b3ad87bc955c4e17b5b98f73a2c4f8f8f5
user.name <nicoescol0818@gmail.com>
2020-05-13 16:42:47 -0500

    add thread classes without body method

---
+++ olimpic-alghoritms/src/thread/ClockMovementThread.java
@@ -0,0 +1,19 @@
+package thread;
+import ui.OlimpicAlghoritmsGUI;
+public class ClockMovementThread extends Thread {
+	
+	//relations
+	
+	private OlimpicAlghoritmsGUI oagui;
+	
+	//methods
+	
+	public ClockMovementThread(OlimpicAlghoritmsGUI gui) {
+		oagui = gui;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+}
