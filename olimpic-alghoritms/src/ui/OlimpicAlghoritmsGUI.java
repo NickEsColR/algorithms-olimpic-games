e87224d15b0ca257cd3e1f89aa6e22ded128a807
user.name <nicoescol0818@gmail.com>
2020-05-13 16:35:53 -0500

    ui classes added, controller in blank

---
+++ olimpic-alghoritms/src/ui/OlimpicAlghoritmsGUI.java
@@ -0,0 +1,14 @@
+package ui;
+import model.Olimpic;
+public class OlimpicAlghoritmsGUI {
+	
+	//relations
+	
+	private Olimpic olimpic;
+
+	public OlimpicAlghoritmsGUI() {
+		olimpic = new Olimpic();
+	}
+	
+	
+}
