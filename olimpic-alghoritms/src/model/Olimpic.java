d747cb5a80a8cd9f30da506d0c347d155ec1f16f
user.name <nicoescol0818@gmail.com>
2020-05-13 19:28:58 -0500

    method add Iterative created

--- olimpic-alghoritms/src/model/Olimpic.java
+++ olimpic-alghoritms/src/model/Olimpic.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Olimpic {
 	
@@ -20,33 +21,82 @@ public class Olimpic {
 		return alAthlete;
 	}
 
-	public void setAlAthlete(ArrayList<Info> alAthlete) {
-		this.alAthlete = alAthlete;
-	}
+
 
 	public LinkedListInfo getFirstLEAthlete() {
 		return firstLEAthlete;
 	}
 
-	public void setFirstLEAthlete(LinkedListInfo firstLEAthlete) {
-		this.firstLEAthlete = firstLEAthlete;
-	}
+
 
 	public TreeInfo getRootAbbAthlete() {
 		return rootAbbAthlete;
 	}
 
-	public void setRootAbbAthlete(TreeInfo rootAbbAthlete) {
-		this.rootAbbAthlete = rootAbbAthlete;
+	
+	
+	public void generateArrayList(long n, boolean recursive) {
+		if(recursive) {
+			generateArrayListRecursive(n);
+		}else {
+			Random random = new Random();
+			for(int i = 0; i < n; i++) {
+				alAthlete.add(new Info(random.nextLong()));
+			}
+		}
 	}
 	
-	public void generateArrayList(long n, boolean recursive) {}
+	public void generateLinkedList(long n, boolean recursive) {
+		if(recursive) {
+			generateLinkedListRecursive(n);
+		}else {
+			Random random = new Random();
+			firstLEAthlete = new LinkedListInfo(random.nextLong());
+			LinkedListInfo addAfter = firstLEAthlete;
+			for(int i = 1; i < n; i++) {
+				addAfter.setNext(new LinkedListInfo(random.nextLong()));
+				addAfter.getNext().setPrev(addAfter);
+				addAfter = addAfter.getNext();
+			}
+		}
+	}
 	
-	public void generateLinkedList(long n, boolean recursive) {}
+	public void generateTree(long n, boolean recursive) {
+		if(recursive) {
+			generateTreeRecursive(n);
+		}else {
+			Random random = new Random();
+			rootAbbAthlete = new TreeInfo(random.nextLong());
+			for(int i = 1; i < n; i++) {
+				long number = random.nextLong();
+				boolean search = true;
+				TreeInfo addAfter = rootAbbAthlete;
+				while(search) {
+					if(addAfter.getRight() != null && addAfter.getLeft() != null) {
+						if(number > addAfter.getNumber()) {
+							addAfter.getRight();
+						}else {
+							addAfter.getLeft();
+						}
+					}else {
+						if(addAfter.getRight() == null && number > addAfter.getNumber()) {
+							addAfter.setRight(new TreeInfo(number));
+							search = false;
+						}else if(addAfter.getRight() == null && number <= addAfter.getNumber()) {
+							addAfter.setLeft(new TreeInfo(number));
+							search = false;
+						}else {
+							addAfter = addAfter.getRight() != null ? addAfter.getRight() : addAfter.getLeft();
+						}
+					}
+				}
+			}
+		}
+	}
 	
-	public void generateTree(long n, boolean recursive) {}
-	
-	public void searchArrayList(long n, boolean recursive) {}
+	public void searchArrayList(long n, boolean recursive) {
+		
+	}
 	
 	public void searchLinkedList(long n, boolean recursive) {}
 	
